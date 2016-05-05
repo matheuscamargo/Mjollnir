96b84ab2f7f95b46d1a6041b55aeb9c4b662be85
Marco Aurelio Prado <marco.pdsv@gmail.com>
2016-05-05 05:58:18 -0300

    Implementing go game logic

--- vigridr/src/games/go/GameLogic.cpp
+++ vigridr/src/games/go/GameLogic.cpp
@@ -4,6 +4,20 @@
 
 namespace mjollnir { namespace vigridr {
 
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/****   GAME_LOGIC   ****/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+
 GameLogic::GameLogic(int32_t playerId1, int32_t playerId2) {
   player1_ = playerId1;
   player2_ = playerId2;
@@ -39,28 +53,68 @@ GameLogic::GameLogic(int32_t playerId1, int32_t playerId2) {
        Marker::UNMARKED,Marker::UNMARKED,Marker::UNMARKED}
   };
   worldModel_.table = table;
+
+  bMove_ = 0;
+  wMove_ = 0;
+
+  //initialize captured stones count to 0
+  blackPrisoners_ = 0;
+  whitePrisoners_ = 0;
+
+  //set both players intial scores
+  scoreBlack_ = 0;
+  scoreWhite_ = 0;
+
+  //set to -1 when there is no KO on the board
+  koPosX_ = -1;
+  koPosY_ = -1;
+
+  blackStonesNumber_ = initialBlackStonesNumber_;
+  whiteStonesNumber_ = initialWhiteStonesNumber_;
+ 
+  //make space for the grid_
+  grid_ = new Stone **[boardSize_];
+  for( int x = 0; x<boardSize_; x++ ){
+    grid_[x] = new Stone *[boardSize_];
+    for( int y = 0; y < boardSize_; y++ ){
+      grid_[x][y] = new Stone( EMPTY, x, y );
+    }
+  }
 }
 
+// TODO: Change logic of /Mjollnir/vigridr/src/server/GameManager.cpp to receive
+// from GemeLogic::update two booleans, one that indicates invalid command and another
+// that indicates end of match
 bool GameLogic::update(Command command, int32_t playerId) {
-  if(!hasFinished_ && isValidMove(command.coordinate)) {
+  if(!hasFinished_ && isValidMove_(command.coordinate.x, command.coordinate.y)) {
     if (playerId == player1_) {
-      if (!isPassMove_(coordinate)) {
+      if (!isPassMove_(command.coordinate.x, command.coordinate.y)) {
         //perform black's move
-        bMove = step(BLACK, command.coordinate);
+        bMove_ = step_(BLACK, command.coordinate);
+        blackStonesNumber_--;
         setTableCoordinate_(command.coordinate, Marker::X);
       }
       else {
-        bMove = -1;
+        bMove_ = -1;
       }
     }
     else if (playerId == player2_) {
-      setTableCoordinate_(command.coordinate, Marker::O);
-      hasFinished_ = checkVictory_(worldModel_, Marker::O , playerId);
+      if (!isPassMove_(command.coordinate.x, command.coordinate.y)) {
+        //perform white's move
+        wMove_ = step_(WHITE, command.coordinate);
+        whiteStonesNumber_--;
+        setTableCoordinate_(command.coordinate, Marker::O);
+      }
+      else {
+        wMove_ = -1;
+      }
     }
-    //check for 2 passes in a row
-    if( bMove == -1 && wMove == -1 ) {
+
+    if( matchFinished_() ) {
       hasFinished_ = true;
+      determineWinner_();
     }
+
     return true;
   }
   return false;
@@ -102,6 +156,7 @@ void GameLogic::setWinner(std::string value) {
   winner_ = value;
 }
 
+// Can delete this method?
 bool GameLogic::randomPlay_(int32_t playerId) {
   for(auto& line : worldModel_.table)
     for(auto& element : line)
@@ -116,57 +171,6 @@ bool GameLogic::randomPlay_(int32_t playerId) {
         }
       }
   return false;
-}
-
-bool GameLogic::checkLines_(const WorldModel& wm, Marker player) {
-  for(const auto& line : wm.table) {
-    for(size_t i = 0; i < boardSize_; ++i) {
-      if(line[i] != player) break;
-      if(i == boardSize_-1) return true;
-    }
-  }
-  return false;
-}
-
-bool GameLogic::checkColumns_(const WorldModel& wm, Marker player) {
-  for(size_t j = 0; j < boardSize_; ++j) {
-    for(size_t i = 0; i < boardSize_; ++i) {
-      if(wm.table[i][j] != player) break;
-      if(i == boardSize_-1) return true;
-    }
-  }
-  return false;
-}
-
-bool GameLogic::checkDiagonals_(const WorldModel& wm, Marker player) {
-  for(size_t i = 0; i < boardSize_; ++i) {
-    if(wm.table[i][boardSize_-1-i] != player) break;
-    if(i == boardSize_-1) return true;
-  }
-  for(size_t i = 0; i < boardSize_; ++i) {
-    if(wm.table[i][i] != player) break;
-    if(i == boardSize_-1) return true;
-  }
-  return false;
-}
-
-bool GameLogic::checkVictory_(const WorldModel& wm, Marker player, int32_t playerId) {
-  if(checkLines_(worldModel_, player) ||
-     checkColumns_(worldModel_, player) ||
-     checkDiagonals_(worldModel_, player) ) {
-    winner_ = std::to_string(playerId);
-    return true;
-  }
-  return false;
-}
-
-bool GameLogic::checkDraw_(const WorldModel& wm) {
-  for(const auto& line : wm.table) {
-    for(size_t i = 0; i < boardSize_; ++i) {
-      if(line[i] == Marker::UNMARKED) return false;
-    }
-  }
-  return true;
 }
 
 bool GameLogic::shouldPrintWorldModel(int32_t playerId){
@@ -195,20 +199,20 @@ GameResult GameLogic::createGameResult(std::string result, int32_t id) {
   return GameResult::LOST;
 }
 
-bool GameLogic::isOnBoard_(const Coordinate& coordinate)
+bool GameLogic::isOnBoard_(int x, int y)
 {
   // check that move is inside the board space
-  if (coordinate.x >= 0 && coordinate.x < boardSize_ &&
+  if (x >= 0 && x < boardSize_ &&
       y >= 0 && y < boardSize_) {
     return true;
   }    
     return false;
 }
 
-bool GameLogic::isKo_(const Coordinate& coordinate)
+bool GameLogic::isKo_(int32_t x, int32_t y)
 {
   // check if move is equivalent to the previous game state
-  if (coordinate.x == koPosX && coordinate.y == koPosY)
+  if (x == koPosX_ && y == koPosY_)
   {
     return true;
   }
@@ -216,30 +220,472 @@ bool GameLogic::isKo_(const Coordinate& coordinate)
 }
 
 bool GameLogic::isPlayer_(
-    const Coordinate& coordinate, Marker marker) {
-  return worldModel_.table[coordinate.x][coordinate.y] == marker;
+    int32_t x, int32_t y, Marker marker) {
+  return worldModel_.table[x][y] == marker;
 }
 
-bool GameLogic::isValidMove_(const Coordinate& coordinate)
+bool GameLogic::isValidMove_(int32_t x, int32_t y)
 {
   // passing turn
-  if (isPassMove_(coordinate))
+  if (isPassMove_(x, y))
     return true;
-  if (isOnBoard_(coordinate) &&
-      isPlayer_(coordinate, Marker::UNMARKED) &&
-      !isKo_(coordinate)) {
+  if (isOnBoard_(x, y) &&
+      isPlayer_(x, y, Marker::UNMARKED) &&
+      !isKo_(x, y)) {
     return true;
   }
   return false;
 }
 
-bool GameLogic::isPassMove_(const Coordinate& coordinate) {
-  return coordinate.x == -1 && coordinate.y == -1;
+bool GameLogic::isPassMove_(int32_t x, int32_t y) {
+  return x == -1 && y == -1;
 }
 
+bool GameLogic::matchFinished_(){
+  //check for 2 passes in a row
+  if( whiteStonesNumber_ == 0 && blackStonesNumber_ == 0 )
+    return true;
+  if( bMove_ == -1 && wMove_ == -1 )
+    return true;
+  return false;
+}
 
+void GameLogic::determineWinner_(){
+  //determine the winner
+  Player winner = scoreArea_();
 
+  switch( winner ){
+    case( BLACK ):
+      winner_ = std::to_string(player1_);
+      break;
+    case( WHITE ):
+      winner_ = std::to_string(player2_);
+      break;
+    default:
+      winner_ = "-1";
+      break;
+  }
+}
 
+/*
+  core function, drives all action on the board each turn
 
+  param player: the player whose turn needs to be executed
+
+  param x,y: coordinates of the stone to be placed
+             (note, should be validated with isValidMove() before
+       step is called.
+   
+  return int: a signal to show the turn has completed succesfully
+*/
+int GameLogic::step_( Player player, const Coordinate& coordinate ){
+  Player currPlayer = player;
+  Player Enemy;
+  Stone *currStone;
+  bool KOflag1 = false;
+  Group playerGroup;
+  int x = coordinate.x;
+  int y = coordinate.y;
+ 
+  std::string playerName;
+
+  //establish the opposing player for reference
+  if( currPlayer == BLACK ){
+    Enemy = WHITE;
+  } 
+  else{
+    Enemy = BLACK;
+  }
+ 
+  //get the stone at x, y
+  currStone = grid_[x][y];
+  
+  //set the piece to its new player
+  currStone->setPlayer( currPlayer ); 
+
+  //test and possibly kill enemies
+  KOflag1 = effectEnemies_( Enemy, x, y ); //set the Ko flag to the output
+
+  //initialize the attributes of the group for the current player
+  playerGroup.size = 0;
+  playerGroup.liberties = 0;
+  playerGroup.player = currPlayer;
+  playerGroup.owner = (Player)EMPTY;
+  //call to determine the attributes
+  playerGroup = getGroup_( x, y, playerGroup );
+
+  //test if player killed themselves
+  if( playerGroup.liberties == 0 ){
+    clearGroup_( currPlayer, x, y );
+  }
+
+  //Determine if there is not a KO
+  if( playerGroup.liberties != 1 || playerGroup.size != 1 || !KOflag1 ){
+    //The KO positions are set when an enemy is killed,
+    //if a KO is not possible then the positions need to be reset 
+    //to an unreachable position -1,-1 before the step finishes
+    koPosX_ = -1;
+    koPosY_ = -1;
+  }
+  
+  //clear all our Counted marks
+  clearMarks_();
+
+  //return to signal that the player made a move
+  return 1;  
+}
+
+/*
+  Probes adjecent stones and determines if any neighbor groups
+  no longer have liberties
+
+  param enemy: the color of the opponents stones
+  param x, y: coordinates of the newly placed stone
+
+  return Bool, KOflag, returns true if a state of KO may be possible
+  this flag, combined with the flag in the main step function, determines
+  if there is in fact a KO state.
+*/
+bool GameLogic::effectEnemies_( Player enemy, int x, int y ){  
+  //flags to see if a state of KO may be possible
+  int killSize = 0; //size of last gorup killed, used to test for KO, and scoring
+  int numKilled = 0; //groups killed, used to test for KO
+
+  //create a group to be reused to test all enemies
+  Group enemyGroup;
+  enemyGroup.player = enemy;
+  enemyGroup.owner = (Player)EMPTY;
+  enemyGroup.size = 0;
+  enemyGroup.liberties = 0;
+
+  //array's to represent orthogonal neighbors
+  int xnew[4] = { x-1, x, x+1, x };
+  int ynew[4] = { y, y-1, y, y+1 };
+
+  //iterate through each neighbor
+  for( int i = 0; i < 4; i++ ){
+    //make sure we don't go outside the board
+    if( isOnBoard_( xnew[i], ynew[i] )){
+      //see if the stone is an enemy and has NOT been counted yet
+      if( grid_[xnew[i]][ynew[i]]->isPlayer( enemy ) && 
+          !grid_[xnew[i]][ynew[i]]->isCounted() ){
+        enemyGroup.size = 0;
+        enemyGroup.liberties = 0;
+        enemyGroup = getGroup_( xnew[i], ynew[i], enemyGroup );
+        if( enemyGroup.liberties == 0 ){
+          //set the killSize when a group is killed
+          killSize = enemyGroup.size;
+
+          numKilled += 1;
+          
+          //sets the KO position temporarily
+          //will be cleared in the step function if
+          //a ko is not present
+          koPosX_ = xnew[i];
+          koPosY_ = ynew[i];
+          
+          switch( enemyGroup.player ){
+            case(WHITE):{
+              blackPrisoners_ += killSize;
+              break;
+            }
+            case(BLACK):{
+              whitePrisoners_ += killSize;
+              break;
+            }
+            default:{
+              break;
+            }
+          }
+          clearGroup_( enemy, xnew[i], ynew[i] );
+        }
+      }
+    }
+  }
+
+  //if a single group of size 1 is killed then we need to return that a
+  //KO may be possible depending on the size of the player group
+  if( numKilled == 1 && killSize == 1){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+/*
+  core function: recursively traces through every stone that is
+  orthogonally connected starting at stone x,y. as it traverses it counts
+  and updates the attributes Size, Liberties, and Owner, and returns them
+  wrapped in a Group structure
+
+  param x,y: coordinates of a stone within a group
+  param currGroup: a copy of a Group object that holds all attributes
+  
+  returns currGroup after traversing all stones in the group
+*/
+Group GameLogic::getGroup_( int x, int y, Group currGroup ){
+  //coordinates of all neighbor stones
+  int xnew[] = { x-1, x, x+1, x };
+  int ynew[] = { y, y-1, y, y+1 };
+
+  //set as counted to prevent double count
+  grid_[x][y]->setCounted();
+
+  //iterate through every neighbor
+  for( int i = 0; i < 4; i++ ){
+    //verify we're on the board
+    if( isOnBoard_( xnew[i], ynew[i] )){
+      //verify we have the same player
+      if( grid_[xnew[i]][ynew[i]]->isPlayer(currGroup.player)){
+        //check if already counted
+        if( !grid_[xnew[i]][ynew[i]]->isCounted()){
+          currGroup = getGroup_( xnew[i], ynew[i], currGroup );
+        }
+      }
+      //if it is not the same player then we have to determine if ownership
+      //of the group has changed for empty territories
+      else if(  currGroup.owner == EMPTY || 
+                grid_[xnew[i]][ynew[i]]->isPlayer(currGroup.owner)){
+        //if we have run into the same owner, or the owner hasn't been set
+        //then the piece we've run into is the owner
+        currGroup.owner = (Player)grid_[xnew[i]][ynew[i]]->getPlayer();
+      }
+      else{
+        //if we've run into something different than the owner
+        //then the territory is contested, once it's marked as DOMI
+        //it can't be scored for either player
+        currGroup.owner = (Player)DOMI;
+      }
+    }
+  }
+
+  //increment the group size and find any liberties next to the current stone
+  currGroup.size += 1;
+  currGroup.liberties += countNeighbors_( EMPTY, x, y );
+
+  return currGroup;
+}
+
+/*
+  Clears a group of stones from the board
+
+  param player: color of the group
+  param x, y: coordinates of a stone within the group
+*/
+void GameLogic::clearGroup_( Player player, int x, int y ) 
+{
+  //clear the current node
+  grid_[x][y]->setPlayer( EMPTY );
+
+  //array's for the orthogonal neighbors
+  int xnew[4] = { x-1, x, x+1, x };
+  int ynew[4] = { y, y-1, y, y+1 };
+
+  //iterate through each neighbor
+  for( int i = 0; i < 4; i++ ){
+    //make sure we don't go outside the board
+    if( isOnBoard_( xnew[i],ynew[i] )){
+      //make sure the stone is of the correct player
+      if( grid_[xnew[i]][ynew[i]]->isPlayer( player )){
+        //clear the stone and any stones of the same player attached to it
+        clearGroup_( player, xnew[i], ynew[i] );
+      }
+    }
+  }
+}
+
+/*
+  clears all of our counted marks on the board
+*/
+void GameLogic::clearMarks_(){
+  //clear all the counted marks on the board
+  for( int x = 0; x < boardSize_; x++ ){
+    for( int y = 0; y < boardSize_; y++ ){
+      grid_[x][y]->clearCounted();
+    }
+  }
+}
+
+/*
+  Counts the all neighbors of a specific player around the
+  provided stone.
+
+  param target: the player type we are counting
+  param x and y: the grid_ coordinates of the center stone
+
+  return count: the number of neighbors of target color
+*/
+int GameLogic::countNeighbors_( Player target, int x, int y ){
+  int count = 0;
+  int xnew[4] = { x-1, x, x+1, x };
+  int ynew[4] = { y, y-1, y, y+1 };
+
+  for( int i = 0; i<4; i++ ){
+    if( isOnBoard_( xnew[i], ynew[i] )){
+      if( grid_[xnew[i]][ynew[i]]->isPlayer(target)){
+        count += 1;
+      }
+    }
+  }
+  return count;
+}
+
+/*
+  Scores the board according to chinese Area scoring rules
+  -each stone on the board is a point for it's owning player
+  -each empty group that is completely surrounded by just 1 player's
+   stones and board edges has its size scored for that player.
+
+   returns the player who has the higher score
+*/
+Player GameLogic::scoreArea_(){
+  
+  Group currGroup;
+
+  //reset the scores
+  scoreBlack_ = 0;
+  scoreWhite_ = 0;
+  
+  //iterate through the board
+  for( int x = 0; x < boardSize_; x++ ){
+    for( int y = 0; y < boardSize_; y++ ){
+      //reset the attributes of the group
+      currGroup.size = 0;
+      currGroup.liberties = 0;
+      currGroup.owner = EMPTY;
+      //don't double count... never double count!
+      if( !grid_[x][y]->isCounted() ){
+        switch( grid_[x][y]->getPlayer() ){
+          //determine the player, set the player of the group
+          //and determine it's size, if one of the players
+          //add it to their score
+          case( BLACK ):
+            currGroup.player = (Player)BLACK;
+            currGroup = getGroup_( x, y, currGroup );
+            scoreBlack_ += currGroup.size;
+            break;
+          case( WHITE ):
+            currGroup.player = (Player)WHITE;
+            currGroup = getGroup_( x, y, currGroup );
+            scoreWhite_ += currGroup.size;
+            break;
+          case( EMPTY ): 
+            //if it's empty we have to figure out who owns the group
+            currGroup.player = (Player)EMPTY;
+            currGroup.owner = (Player)EMPTY;
+            currGroup = getGroup_( x, y, currGroup);
+            //figure out the owner of the controlled territory
+            //add it to their score
+            switch( currGroup.owner ){
+              case( BLACK ):
+                scoreBlack_ += currGroup.size;
+                break;
+              case( WHITE ):
+                scoreWhite_ += currGroup.size;
+                break;
+              default:
+                break;
+            }     
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  clearMarks_();
+
+  //find the bigger number
+  //return the winner
+  if( scoreWhite_ > scoreBlack_ ){
+    return WHITE;
+  }
+  else if( scoreBlack_ > scoreWhite_ ){
+    return BLACK;
+  }
+  else{
+    return EMPTY;
+  }
+}
+
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/****     STONE      ****/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+
+Stone::Stone( Player player, int x, int y ) 
+{
+  this->y = y;
+  this->x = x;
+  this->player = player;
+  Counted = 0;
+}
+
+//group of get/set methods
+void Stone::setY(int y) 
+{
+  this->y = y;
+}
+
+void Stone::setX(int x) 
+{
+  this->x = x;
+}
+
+void Stone::setPlayer( Player player ) 
+{
+  this->player = player;
+}
+  
+char Stone::getPlayer() 
+{
+  return player;
+}
+
+bool Stone::isPlayer( Player target )
+{
+  if( target == player )
+    {
+      return true;
+    }
+  return false;
+}
+
+int Stone::getX() 
+{
+  return x;
+}
+
+int Stone::getY() 
+{
+  return y;
+}
+
+//set up to only allow boolean access to Counted variable
+void Stone::setCounted() 
+{
+  Counted = true;
+}
+
+void Stone::clearCounted() 
+{
+  Counted = false;
+}
+
+bool Stone::isCounted() 
+{
+  return Counted;
+}
 
 }}  // namespaces
--- vigridr/src/games/go/GameLogic.h
+++ vigridr/src/games/go/GameLogic.h
@@ -9,7 +9,80 @@
 
 namespace mjollnir { namespace vigridr {
 
+typedef enum Player_t {
+  BLACK = 'B',
+  WHITE = 'W',
+  EMPTY = '.',
+  DOMI = 'D'
+} Player;
+
 struct TotalWorldModel {};
+
+typedef struct Group_t{
+  int size;
+  int liberties;
+  Player player;
+  Player owner;
+} Group;
+
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/****     STONE      ****/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+
+class Stone {
+
+ private:
+  int x;
+  int y;
+  Player player;
+  bool Counted; 
+
+ public:
+  Stone() {Counted = 0;};
+  ~Stone() {};
+  
+  Stone(Player player, int x, int y);
+
+  //control for the counted flag
+  void clearCounted();
+  void setCounted();
+  bool isCounted();
+  
+  //get/set and boolean test for the Player
+  void setPlayer(Player player);
+  char getPlayer();
+  bool isPlayer(Player target);
+  
+  //Getter/setters for coordinates
+  void setY(int y);
+  void setX(int x);
+  int getX();
+  int getY();
+};
+
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/****   GAME_LOGIC   ****/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
+/************************/
 
 class GameLogic {
  public:
@@ -42,27 +115,40 @@ class GameLogic {
   GameResult createGameResult(std::string result, int32_t id);
 
  private:
-  bool checkLines_(const WorldModel& wm, Marker player);
-  bool checkColumns_(const WorldModel& wm, Marker player);
-  bool checkDiagonals_(const WorldModel& wm, Marker player);
-  bool checkVictory_(const WorldModel& wm, Marker player, int32_t playerId);
-  bool checkDraw_(const WorldModel& wm);
   void setTableCoordinate_(const Coordinate& coordinate, Marker marker);
   bool checkTableCoordinate_(const Coordinate& coordinate, Marker marker);
-  bool isPassMove_(const Coordinate& coordinate);
-  bool isValidMove_(const Coordinate& coordinate);
-  bool isPlayer_(const Coordinate& coordinate, Marker marker);
-  bool isKo_(const Coordinate& coordinate);
-  bool isOnBoard_(const Coordinate& coordinate);
+  bool isPassMove_(int32_t x, int32_t y);
+  bool isValidMove_(int32_t x, int32_t y);
+  bool isPlayer_(int32_t x, int32_t y, Marker marker);
+  bool isKo_(int32_t x, int32_t y);
+  bool isOnBoard_(int32_t x, int32_t y);
   bool randomPlay_(int32_t playerId);
+  bool matchFinished_();
+  void determineWinner_();
+  int step_( Player player, const Coordinate& coordinate );
+  bool effectEnemies_( Player enemy, int x, int y );
+  int countNeighbors_( Player target, int x, int y );
+  void clearMarks_();
+  void clearGroup_( Player player, int x, int y );
+  Group getGroup_( int x, int y, Group currGroup );
+  Player scoreArea_();
   WorldModel worldModel_;
   TotalWorldModel twm_;
   int32_t player1_, player2_;
-  int32_t koPosX, koPosY;
-  int32_t bMove, wMove;
+  int32_t koPosX_, koPosY_;
+  int32_t bMove_, wMove_;
   std::string winner_;
   bool hasFinished_;
-  const size_t boardSize_ = 9;
+  Stone *** grid_; //2d array of stone pointers
+  int32_t scoreBlack_;
+  int32_t scoreWhite_;
+  int32_t blackPrisoners_;
+  int32_t whitePrisoners_;
+  int32_t blackStonesNumber_;
+  int32_t whiteStonesNumber_;
+  const int32_t boardSize_ = 9;
+  const int32_t initialBlackStonesNumber_ = 41;
+  const int32_t initialWhiteStonesNumber_ = 40;
   const size_t numberOfPlayers_ = 2;
 };
 
