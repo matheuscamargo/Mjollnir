29ac2a8ff4f744bca7a3249ff9660a81a5f4a3d5
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-05-06 02:03:23 -0300

    Added currently desired mocked tournament

--- sources/TournamentSource.js
+++ sources/TournamentSource.js
@@ -1,5 +1,42 @@
 var mockData = {
-  name: "Test Tournament"
+  name: "Test Tournament",
+  sections: [
+    {
+      id: 1,
+      rounds: [
+        {
+          id: 1,
+          mathes: [
+            {
+              id: 1,
+              players: ['Mut', 'Peh'],
+              scores: [1, 0],
+            },
+            {
+              id: 2,
+              players: ['Gafa', 'Baya'],
+              scores: [1, 0],
+            },
+          ],
+        },
+        {
+          id: 2,
+          mathes: [
+            {
+              id: 1,
+              players: ['Mut', 'Peh'],
+              scores: [1, 0],
+            },
+            {
+              id: 2,
+              players: ['Gafa', 'Baya'],
+              scores: [1, 0],
+            },
+          ],
+        },
+      ],
+    }
+  ],
 };
 
 class TournamentSource {
