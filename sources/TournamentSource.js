d8d4ac23e0ecefdce1a060b93042700e6788d462
LuizFelipeh <luizfelipeh95@gmail.com>
2016-05-06 05:36:30 -0300

    Duel tournament almost implemented

--- sources/TournamentSource.js
+++ sources/TournamentSource.js
@@ -1,43 +1,7 @@
 var mockData = {
-  name: "Test Tournament",
-  sections: [
-    {
-      id: 1,
-      rounds: [
-        {
-          id: 1,
-          matches: [
-            {
-              id: {s: 1, r: 1, m: 1},
-              players: ['Mut', 'Peh'],
-              scores: [1, 0],
-            },
-            {
-              id: {s: 1, r: 1, m: 2},
-              players: ['Gafa', 'Baya'],
-              scores: [1, 0],
-            },
-          ],
-        },
-        {
-          id: 2,
-          matches: [
-            {
-              id: {s: 1, r: 2, m: 1},
-              players: ['Mut', 'Peh'],
-              scores: [1, 0],
-            },
-            {
-              id: {s: 1, r: 2, m: 2},
-              players: ['Gafa', 'Baya'],
-              scores: [1, 0],
-            },
-          ],
-        },
-      ],
-    }
-  ],
-};
+  Players: ["Mut", "Peh", "Gafa", "Bay", "Philip", "Josu", "Caique", "Mil"],
+  nPlayers: 8,
+  };
 
 class TournamentSource {
   fetch() {
--- stores/TournamentStore.js
+++ stores/TournamentStore.js
@@ -1,6 +1,9 @@
 var Duel = require('duel');
+var _ = require('underscore');
 import alt from '../alt';
 import TournamentActions from '../actions/TournamentActions';
+
+
 
 class TournamentStore{
   constructor(){
@@ -11,16 +14,23 @@ class TournamentStore{
       handleCreate: TournamentActions.CREATE,
       handleFetch: TournamentActions.FETCH,
       handleFetchFail: TournamentActions.FETCH_FAILED,
-      handlePlayMatch: TournamentActions.PLAY_MATCH
+      handlePlayMatch: TournamentActions.PLAY_MATCH,
     });
   }
 
-  handleCreate(tournamentInfo){
-    var DuelTournament = new Duel(TournamentInfo.nPlayers);
+  handleCreate(TournamentInfo){
+    var DuelTournament = new Duel(TournamentInfo.Players.length);
     DuelTournament.matches.forEach(function (m) {
       console.log(m.id);
     });
-    console.log("Tournament: " + DuelTournament.matches);
+
+    var OrganizeBySection = _.groupBy(DuelTournament.matches,  function(num){ return num.id.s;});
+    var DesiredSchema = _.map(OrganizeBySection, function(value, key)
+                        { return { id: key, rounds: _.map(_.groupBy(value, function(num){ return num.id.r;}), function(value, key)
+                        { return { id: key, matches: _.map(value, function(num){return {id: num.id, players: [TournamentInfo.Players[num.p[0] - 1], TournamentInfo.Players[num.p[1] - 1]]}})}})}});
+
+    console.log(DesiredSchema);
+
   }
 
   handleFetch() {
@@ -34,6 +44,7 @@ class TournamentStore{
   handlePlayMatch(matchInfo){
     console.log(matchInfo);
   }
+
 }
 
 export default alt.createStore(TournamentStore, 'TournamentStore');
