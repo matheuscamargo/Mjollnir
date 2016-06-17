5d2013a4c65eac93978368fee5a0b28b2a4f6aef
LuizFelipeh <luizfelipeh95@gmail.com>
2016-06-17 06:29:05 -0300

    Initial test

--- bifrost/ragnarok/sources/UserSource.js
+++ bifrost/ragnarok/sources/UserSource.js
@@ -2,13 +2,13 @@ import _ from 'underscore';
 
 var mockData = [
   {id: 0, name: 'Mut', ranking: 123456},
-  {id: 1, name: 'Peh', ranking: 10},
+  {id: 1, name: 'victorvpascoal', ranking: 10},
   {id: 2, name: 'Gafa', ranking: 8},
   {id: 3, name: 'Philip', ranking: 5},
   {id: 4, name: 'Bay', ranking: 3},
   {id: 5, name: 'Braile', ranking: 6},
   {id: 6, name: 'Josu', ranking: 7},
-  {id: 7, name: 'Mil', ranking: 6},
+  {id: 7, name: 'Mut', ranking: 6},
   {id: 8, name: 'victorvpascoal', ranking: 9},
 ];
 
--- bifrost/static/js/bundle/bundle.js
+++ bifrost/static/js/bundle/bundle.js
@@ -25584,7 +25584,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var mockData = [{ id: 0, name: 'Mut', ranking: 123456 }, { id: 1, name: 'Peh', ranking: 10 }, { id: 2, name: 'Gafa', ranking: 8 }, { id: 3, name: 'Philip', ranking: 5 }, { id: 4, name: 'Bay', ranking: 3 }, { id: 5, name: 'Braile', ranking: 6 }, { id: 6, name: 'Josu', ranking: 7 }, { id: 7, name: 'Mil', ranking: 6 }, { id: 8, name: 'victorvpascoal', ranking: 9 }];
+var mockData = [{ id: 0, name: 'Mut', ranking: 123456 }, { id: 1, name: 'victorvpascoal', ranking: 10 }, { id: 2, name: 'Gafa', ranking: 8 }, { id: 3, name: 'Philip', ranking: 5 }, { id: 4, name: 'Bay', ranking: 3 }, { id: 5, name: 'Braile', ranking: 6 }, { id: 6, name: 'Josu', ranking: 7 }, { id: 7, name: 'Mut', ranking: 6 }, { id: 8, name: 'victorvpascoal', ranking: 9 }];
 
 var UserSource = function () {
   function UserSource() {
