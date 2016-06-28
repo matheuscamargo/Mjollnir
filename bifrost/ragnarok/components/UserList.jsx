b23d03e4ad8ad1d6ba3dd69492650450bb4c0a99
LuizFelipeh <luizfelipeh95@gmail.com>
2016-06-28 00:39:03 -0300

    Classic

--- bifrost/ragnarok/components/UserList.jsx
+++ bifrost/ragnarok/components/UserList.jsx
@@ -128,8 +128,8 @@ export default class MyComponent extends React.Component {
           </div>
           <br>
           <div>
-            <input type="button" className="btn btn-default" value="Start Tournament" hidden={selectedUsers.length < MIN_USERS} onClick={(e) => this._handleClickTournament(e, selectedUsers)}/>
-            <span className="alert alert-danger" hidden={!(selectedUsers.length < MIN_USERS)}> Número mínimo de participantes no campeonato é {MIN_USERS}. </span>
+            <input type="button" className="btn btn-default" value="Start Tournament" disabled={selectedUsers.length < MIN_USERS} onClick={(e) => this._handleClickTournament(e, selectedUsers)}/>
+            <span color="red" hidden={!(selectedUsers.length < MIN_USERS)}> Número mínimo de participantes no campeonato é {MIN_USERS}. </span>
           </div>
         </div>
       </div>);
